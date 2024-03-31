8a9acaf97d35c8b0472764ef82dc38daa744404e
omarmohamedattia <162788910+omarmohamedattia@users.noreply.github.com>
2024-03-31 18:56:29 +0200

    Add files via upload

---
+++ src/kidcalc/index.js
@@ -0,0 +1,6 @@
+function BackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+function addToOutput(value){
+    document.getElementsByClassName ("output") [0].value += value
+}
